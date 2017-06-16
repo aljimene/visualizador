fa34bdb83c67169d784e9b24a88dcd38b55e17b4
Alberto Jimenez <alberto.jimenez@cti.espol.edu.ec>
2017-06-16 14:43:07 -0500

    Modificación para mostrar archivo en version html

--- js/visualizator.js
+++ js/visualizator.js
@@ -1,42 +1,15 @@
 $(document).ready(function(){
     $("#bt_visualizator").click(function () {
-        $("#ath_vs").empty();
-        $("#txt_vs").empty();	
+        $("#aplication_web").empty();
 	var document_type = $("#document_type").val();
 	var document_id = $("#document_id").val();
-	var url_ws = 'http://localhost:8080/get_analysts/'+document_type+"/"+document_id;
+	var url_ws = 'http://200.10.150.238:8080/get_analysts/'+document_type+"/"+document_id;
 	$("body").addClass("loading");
 	$.getJSON(url_ws, {})
        	  .done(function( data ) {
 		if(data.status==200){
-			html_file = data.html;
-			obj = $.parseJSON(data.data);
-			authors = obj.authors;
-			text_authors = obj.text_authors;
-			text = obj.text;
-			authors_html = "<table border='1'>";
-			var hash_authors = {};
-			$.each( authors, function( index ) {
-			    //alert(this.text());
-			    author = authors[index];
-			    hash_authors[author[1]]=index;
-			    prc = parseFloat(author[3])*100;
-			    authors_html+="<tr><td class='normal' id='td_ath_" + index + "'><a onclick='getContribution(this.id)' id='ath_" + index + "'>" + author[1] + "</a></td><td>"+prc.toFixed(2)+"%</td></tr>";
-			    
-			});
-			authors_html+="</table>";
-			$("#ath_vs").append(authors_html);
-			text_html = "";
-			$.each( text, function( index ) {
-				txt = text[index];
-				if(txt=='enter'){
-					text_html+="<br/>"				
-				}else{
-					text_html+="<span class='normal' id='txt_ath_"+hash_authors[text_authors[index]]+"'>"+txt+" </span>"				
-				}
-			});
-			$("#txt_vs").append(html_file);
-			$("#txt_sin_vs").append(text_html);
+			html_file = data.result;
+			$("#aplication_web").append(html_file);
 			$("body").removeClass("loading");
 		}else{
 			alert( "error" );
